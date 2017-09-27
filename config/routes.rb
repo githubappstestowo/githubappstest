4fe603ba65f5ac0425f9c47668a82547ee2cf103
Patryk Tyszka <patryk@codequest.com>
2017-09-27 21:07:17 +0200

    add home show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+
+  resource :home, only: [:show], controller: :home
   namespace :api, format: :json do
     resources :auth, only: [:create]
   end
